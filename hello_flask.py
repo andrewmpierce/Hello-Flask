d08ee76a6060511e20001dc57fe119fe644615fd
apierce1992 <apierce1992@gmail.com>
2015-12-06 16:50:40 -0500

    Can make tweets in shell

--- hello_flask.py
+++ hello_flask.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask import render_template
 from flask.ext.sqlalchemy import SQLAlchemy
+import datetime as dt
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object('config.Config')
 db = SQLAlchemy(app)
 
 
@@ -10,6 +11,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
+    tweets = db.relationship('Tweet')
 
     def __init__(self, username, email):
         self.username = username
@@ -17,6 +19,21 @@ class User(db.Model):
 
     def __repr__(self):
         return 'User {}'.format(self.username)
+
+
+class Tweet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(140), unique=False)
+    user = db.Column(db.Integer, db.ForeignKey('user.id'))
+    datetime = db.Column(db.DateTime)
+
+    def __init__(self, text, user):
+        self.text = text
+        self.user = user
+        self.datetime = dt.datetime.now()
+
+    def __repr__(self):
+        return '{} by {} at {}'.format(self.text, self.user, self.datetime)
 
 
 
