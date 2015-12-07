23a8567af1ba574b030d834997468b908965f079
apierce1992 <apierce1992@gmail.com>
2015-12-07 10:57:52 -0500

    added flask migrate to requirements

--- hello_flask.py
+++ hello_flask.py
@@ -1,39 +1,46 @@
 from flask import Flask
 from flask import render_template
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
 import datetime as dt
+
 app = Flask(__name__)
 app.config.from_object('config.Config')
 db = SQLAlchemy(app)
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True, autoincrement = True)
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
-    tweets = db.relationship('Tweet')
 
     def __init__(self, username, email):
         self.username = username
         self.email = email
 
     def __repr__(self):
-        return 'User {}'.format(self.username)
+        return '{}'.format(self.username)
 
 
 class Tweet(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'tweet'
+    id = db.Column(db.Integer, primary_key=True, autoincrement = True)
     text = db.Column(db.String(140), unique=False)
-    user = db.Column(db.Integer, db.ForeignKey('user.id'))
     datetime = db.Column(db.DateTime)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    author = db.relationship("User", backref = 'tweets')
 
-    def __init__(self, text, user):
+
+
+    def __init__(self, text, author):
         self.text = text
-        self.user = user
+        self.author = author
         self.datetime = dt.datetime.now()
 
     def __repr__(self):
-        return '{} by {} at {}'.format(self.text, self.user, self.datetime)
+        return '{} by {} at {}'.format(self.text, self.author, self.datetime)
 
 
 
